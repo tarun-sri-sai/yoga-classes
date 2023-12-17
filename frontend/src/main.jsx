0a126c9070596666ae5533b0dcbf7a1a2672e899
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2023-12-17 19:54:29 +0530

    Remove StrictMode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,7 @@ import "./index.css";
 import { UserContextProvider } from "./contexts/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <UserContextProvider>
       <App />
     </UserContextProvider>
-  </React.StrictMode>
 );
